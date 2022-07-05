387a0222eddf55d58b2da927e25f9eec399ff9ca
PrantaChakraborty <pranta.codesmith@gmail.com>
2022-07-05 17:27:15 +0600

    add meta class

--- world/models.py
+++ world/models.py
@@ -22,3 +22,6 @@ class WorldBorder(models.Model):
     # Returns the string representation of the model.
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ['name']
