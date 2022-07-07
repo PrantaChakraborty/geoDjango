c901900c4d2c25fa79cc78fc629a0f3ae0e0a4ca
PrantaChakraborty <pranta.codesmith@gmail.com>
2022-07-07 15:37:06 +0600

    added feature for user input location

--- shop/views.py
+++ shop/views.py
@@ -2,21 +2,37 @@ from django.views import generic
 from django.contrib.gis.geos import fromstr, Point
 from django.contrib.gis.db.models.functions import Distance
 
+from geopy.geocoders import Nominatim
+
+from django.shortcuts import render, HttpResponse
+
 
 from .models import Shop
 
-latitude = 23.7931353
-longitude = 90.4056248
 
-user_location = Point(longitude, latitude, srid=4326)
-
-
-class NearestShopView(generic.ListView):
+class NearestShopView(generic.View):
     """
     return nearest 10 shops based on user
     """
-    model = Shop
-    context_object_name = 'shops'
-    queryset = Shop.objects.annotate(distance=Distance('location', user_location)).order_by('distance')[0:10]
     template_name = 'index.html'
+
+    def get(self, request):
+        return render(request, self.template_name, context={})
+
+    def post(self, request):
+        # try:
+        address = request.POST['address']
+        geo_locator = Nominatim(user_agent='shop')
+        location = geo_locator.geocode(address)
+        if location is not None:
+            latitude = location.latitude
+            longitude = location.longitude
+            user_location = Point(longitude, latitude, srid=4326)
+            queryset = Shop.objects.annotate(distance=Distance('location', user_location)).order_by('distance')[
+                       0:10]
+            return render(request, self.template_name, context={"shops": queryset})
+        else:
+            return render(request, 'not_found.html', context={})
+
+
 
